53348f5869759cb2d2d80c3175158c8092369eb8
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-06-30 16:03:29 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,5 +1,5 @@
 Blockly.Python['map'] = function(block) {
-Blockly.Python.definitions_['add_def'] = 'def map(value, istart, istop, ostart, ostop):\r\n\treturn (x - in_min) * (out_max - out__min) / (in_max - in_min) out_min ';
+Blockly.Python.definitions_['add_def'] = 'def map(value, istart, istop, ostart, ostop):\r\n\treturn (value - istar) * (ostop - ostart) / (istop - istart) ostart ';
 
   //var dropdown_trig = block.getFieldValue('trig');
   var value = Blockly.Python.valueToCode(block, 'value', Blockly.Python.ORDER_ATOMIC);
