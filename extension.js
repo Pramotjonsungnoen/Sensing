8e967146aa434e0692ac1b4e9e789923ea58ec83
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-07-07 09:08:35 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,7 +10,7 @@
         {
             xml: `
                 <block type="map">
-                    <value name="value">
+                    <value name="value ">
                         <shadow type="math_number">
                             <field name="NUM">32</field>
                         </shadow>
